cd7c196515bcc90805a4aa86801f48ddd03692db
Agrizzy6 <145580684+Agrizzy6@users.noreply.github.com>
2023-11-12 20:47:29 -0500

    Add files via upload

--- p4-web/List.hpp
+++ p4-web/List.hpp
@@ -5,12 +5,9 @@
  * doubly-linked, double-ended list with Iterator interface
  * EECS 280 Project 4
  */
-
 #include <iostream>
 #include <cassert> //assert
 #include <cstddef> //NULL
-
-
 template <typename T>
 class List {
   //OVERVIEW: a doubly-linked, double-ended list with Iterator interface
@@ -109,7 +106,6 @@ public:
   // and overloaded assignment operator, if appropriate. If these operations
   // will work correctly without defining these, you can omit them. A user
   // of the class must be able to create, copy, assign, and destroy Lists
-
 private:
   //a private type
   struct Node {
@@ -122,23 +118,18 @@ private:
   //REQUIRES: list is empty
   //EFFECTS:  copies all nodes from other to this
   void copy_all(const List<T> &other);
-
   Node *first;   // points to first Node in list, or nullptr if list is empty
   Node *last;    // points to last Node in list, or nullptr if list is empty
-
 public:
   ////////////////////////////////////////
   class Iterator {
     //OVERVIEW: Iterator interface to List
-
     // You should add in a default constructor, destructor, copy constructor,
     // and overloaded assignment operator, if appropriate. If these operations
     // will work correctly without defining these, you can omit them. A user
     // of the class must be able to create, copy, assign, and destroy Iterators.
-
     // Your iterator should implement the following public operators: *,
     // ++ (prefix), default constructor, == and !=.
-
   public:
     // This operator will be used to test your code. Do not modify it.
     // Requires that the current element is dereferenceable.
@@ -147,44 +138,74 @@ public:
       node_ptr = node_ptr->prev;
       return *this;
     }
+    
+    Iterator& operator++() {
+      assert(node_ptr);
+      node_ptr = node_ptr->next;
+      return *this;
+    }
+
+    T & operator*() const{
+      assert(node_ptr);
+      return node_ptr->datum;
+    }
+
+    bool operator==(Iterator rhs) const{
+      assert(node_ptr);
+      if(*this->node_ptr == *rhs->node_ptr){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+    bool operator!=(Iterator rhs) const{
+      assert(node_ptr);
+      if(*this->node_ptr == *rhs->node_ptr){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    
+
+    Iterator()
+    : node_ptr(this->first){}
+
 
   private:
     Node *node_ptr; //current Iterator position is a List node
     // add any additional necessary member variables here
-
     // add any friend declarations here
-
     // construct an Iterator at a specific position
-    Iterator(Node *p);
+    Iterator(Node *p) 
+    : node_ptr(p){}
 
   };//List::Iterator
   ////////////////////////////////////////
-
   // return an Iterator pointing to the first element
   Iterator begin() const {
     return Iterator(first);
   }
-
   // return an Iterator pointing to "past the end"
-  Iterator end() const;
-
+  Iterator end() const{
+    return Iterator(last);
+  }
   //REQUIRES: i is a valid, dereferenceable iterator associated with this list
   //MODIFIES: may invalidate other list iterators
   //EFFECTS: Removes a single element from the list container
-  void erase(Iterator i);
-
+  void erase(Iterator i){
+    
+  }
   //REQUIRES: i is a valid iterator associated with this list
   //EFFECTS: inserts datum before the element at the specified position.
   void insert(Iterator i, const T &datum);
-
 };//List
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Add your member function implementations below or in the class above
 // (your choice). Do not change the public interface of List, although you
 // may add the Big Three if needed.  Do add the public member functions for
 // Iterator.
-
-
 #endif // Do not remove this. Write all your code above this line.
